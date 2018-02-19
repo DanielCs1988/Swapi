d14f467fc23db8fc46f0c5c42ae03efebd2dced6
Emperor <daniel.cs20@gmail.com>
2018-02-19 20:05:03 +0100

    Added routes for user system.

--- data_handler.py
+++ data_handler.py
@@ -7,3 +7,23 @@ class DataBase:
     def __init__(self):
         self.connection = Connector.connection_from_envvars()
         self.cursor = self.connection.cursor
+
+    def check_user(self, username, password):
+        self.cursor.execute("SELECT password FROM users WHERE username = %(usn)s", {'usn': username})
+        user_entry = self.cursor.fetchone()
+
+        if not user_entry:
+            return False
+
+        return bcrypt.checkpw(password.encode('utf-8'), user_entry['password'].encode('utf-8'))
+
+    def register_user(self, username, password):
+        self.cursor.execute("SELECT username FROM users WHERE username = %(usn)s", {'usn': username})
+        if self.cursor.fetchone():
+            return False
+
+        hashed_pw = bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
+        self.cursor.execute("INSERT INTO users (username, password) VALUES (%(usn)s, %(pw)s)", {
+            'usn': username, 'pw': hashed_pw.decode('utf-8')
+        })
+        return True
--- server.py
+++ server.py
@@ -1,12 +1,40 @@
-from flask import Flask, render_template, request, url_for, redirect
-import data_handler
+from flask import Flask, render_template, request, session
+from data_handler import DataBase
+import json
+import os
 
+db = DataBase()
 app = Flask(__name__)
+
+app.secret_key = os.environ.get('SESSION_KEY')
 
 
 @app.route('/')
 def main():
     return render_template('index.html')
+
+
+@app.route('/login', methods=['POST'])
+def login():
+    login_valid = db.check_user(request.form['username'], request.form['password'])
+    if login_valid:
+        session['current_user'] = request.form['username']
+    return json.dumps(login_valid)
+
+
+@app.route('/logout')
+def logout():
+    if session.get('current_user'):
+        session.pop('current_user', None)
+    return ''
+
+
+@app.route('/register', methods=['POST'])
+def register():
+    reg_valid = db.register_user(request.form['username'], request.form['password'])
+    if reg_valid:
+        session['current_user'] = request.form['username']
+    return json.dumps(reg_valid)
 
 
 if __name__ == '__main__':
